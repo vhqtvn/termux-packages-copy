1240c891103617008a0bf60334e5753cf7a48f3f
vhnvn <vh@fake.com>
2021-07-09 17:17:01 +0700

    update

--- packages-vh-override/swift/build.sh
+++ packages-vh-override/swift/build.sh
@@ -1,6 +1,5 @@
 save_function termux_step_post_get_source termux_step_post_get_source_bk
 
-
 termux_step_post_get_source() {
     termux_step_post_get_source_bk
     rm -rf "$TERMUX_PKG_HOSTBUILD_DIR"
