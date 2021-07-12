f747cee04d507dfe88f25b25681896ae45e6412d
vhnvn <vh@fake.com>
2021-07-12 18:58:39 +0700

    sync

--- packages/k9s/build.sh
+++ packages/k9s/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://k9scli.io
 TERMUX_PKG_DESCRIPTION="Kubernetes CLI To Manage Your Clusters In Style!"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.24.13
+TERMUX_PKG_VERSION=0.24.14
 TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=160733f7546c3d379625dbffd809d098fabc7785a2a09749abc5daefee3e7e5a
+TERMUX_PKG_SHA256=78e432824adb7ba6300784630f9cf371a379cc809b1cbb66aea0cc4fa07b3cf3
 
 termux_step_make() {
         termux_setup_golang
@@ -13,7 +13,7 @@ termux_step_make() {
         cp -a "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s"
         cd "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s"
 
-        go get -d -v
+        termux_go_get -d -v
         go build
 }
 
