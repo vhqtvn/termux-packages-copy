9280599c52c99e7ffaa1cee7a63fd9310110c8b7
vhnvn <vh@fake.com>
2022-03-25 20:14:12 +0700

    trigger apt

--- packages-vh-override/dpkg/build.sh
+++
@@ -1,5 +0,0 @@
-if [[ "$TERMUX_PKG_VERSION" == "1.21.1" ]]; then
-    TERMUX_PKG_VERSION=1.21.3
-    TERMUX_PKG_SRCURL=https://mirrors.kernel.org/debian/pool/main/d/dpkg/dpkg_${TERMUX_PKG_VERSION}.tar.xz
-    TERMUX_PKG_SHA256=668222141dde0f352eee75d46b07f5f1cf3b9550464b54c2287bdd15650c499a
-fi
