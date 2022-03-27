1c0f33d81254255d2fba0d9de987dd6e20ae487b
vhnvn <vh@fake.com>
2022-03-28 01:31:57 +0700

    trigger netpbm

--- packages-vh-override/netpbm/build.sh
+++ packages-vh-override/netpbm/build.sh
@@ -1,6 +1,6 @@
-save_function termux_step_make termux_step_make_bk
-termux_step_make() {
-    sed -i'' 's/swab/s_w_a_b___/g' "$TERMUX_PKG_SRCDIR/converter/other/cameratopam/camera.c"
-    sed -i'' 's/swab/s_w_a_b___/g' "$TERMUX_PKG_SRCDIR/converter/other/cameratopam/util.c"
-	termux_step_make_bk
-}
+# save_function termux_step_make termux_step_make_bk
+# termux_step_make() {
+#     sed -i'' 's/swab/s_w_a_b___/g' "$TERMUX_PKG_SRCDIR/converter/other/cameratopam/camera.c"
+#     sed -i'' 's/swab/s_w_a_b___/g' "$TERMUX_PKG_SRCDIR/converter/other/cameratopam/util.c"
+# 	termux_step_make_bk
+# }
