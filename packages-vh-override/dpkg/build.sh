23f8b769e80aaa942689d591ac233bb0322b9d50
vhnvn <vh@fake.com>
2022-03-27 15:31:41 +0700

    update

--- packages-vh-override/dpkg/build.sh
+++ packages-vh-override/dpkg/build.sh
@@ -1,6 +1,7 @@
 if [[ "$TERMUX_PKG_VERSION" == "1.21.1" ]]; then
     TERMUX_VH_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
     TERMUX_PKG_VERSION=1.21.3
+    TERMUX_PKG_REVISION=0$TERMUX_PKG_REVISION
     TERMUX_PKG_SRCURL=https://mirrors.kernel.org/debian/pool/main/d/dpkg/dpkg_${TERMUX_PKG_VERSION}.tar.xz
     TERMUX_PKG_SHA256=668222141dde0f352eee75d46b07f5f1cf3b9550464b54c2287bdd15650c499a
     save_function termux_step_patch_package termux_step_patch_package_bk
