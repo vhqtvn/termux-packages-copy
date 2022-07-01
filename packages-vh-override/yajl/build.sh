9955bce22dce7aa36b03128df0c02aa9a037a3dd
vhnvn <vh@fake.com>
2022-07-01 15:50:09 +0700

    update from [PrevCommit:37ebdc4c3a745a9f7045e3789ae56ebb556f0bc0]

--- packages-vh-override/yajl/build.sh
+++ packages-vh-override/yajl/build.sh
@@ -1,4 +1,4 @@
 if [[ "$TERMUX_PKG_VERSION" == "2.1.0" ]]; then
     TERMUX_PKG_SRCURL=https://github.com/lloyd/yajl/archive/66cb08ca2ad8581080b626a75dfca266a890afb2.tar.gz
-    TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+    TERMUX_PKG_SHA256=ef6137161734432d847faa11f07ebf1aea05f253225c8ea96cb20c628deead57
 fi
