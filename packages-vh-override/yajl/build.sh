479fe6c228215e68de9fe6318ea33f5d9b71cba2
vhnvn <vh@fake.com>
2022-07-01 15:42:17 +0700

    trigger yajl

--- packages-vh-override/yajl/build.sh
+++ packages-vh-override/yajl/build.sh
@@ -1,3 +1,4 @@
 if [[ "$TERMUX_PKG_VERSION" == "2.1.0" ]]; then
+    TERMUX_PKG_SRCURL=https://github.com/lloyd/yajl/archive/66cb08ca2ad8581080b626a75dfca266a890afb2.tar.gz
     TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 fi
