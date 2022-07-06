566f8bcc59ba981e9bb6eb49062cfe28fa8ba12f
vhnvn <vh@fake.com>
2022-07-07 00:31:05 +0700

    triigger apt

--- packages-vh-override/apt/build.sh
+++ packages-vh-override/apt/build.sh
@@ -1,5 +1,5 @@
-if [[ -z "$TERMUX_PKG_DEPENDS" ]]; then
-    TERMUX_PKG_DEPENDS="zstd"
-else
-    TERMUX_PKG_DEPENDS+=", zstd"
-fi
+#if [[ -z "$TERMUX_PKG_DEPENDS" ]]; then
+#    TERMUX_PKG_DEPENDS="zstd"
+#else
+#    TERMUX_PKG_DEPENDS+=", zstd"
+#fi
