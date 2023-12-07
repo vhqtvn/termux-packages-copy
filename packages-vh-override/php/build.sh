d5719d716165e5b58de854ed73a384e00f907959
vhnvn <vh@fake.com>
2023-12-08 03:48:45 +0700

    trigger php

--- packages-vh-override/php/build.sh
+++ packages-vh-override/php/build.sh
@@ -1,1 +1,6 @@
-#TERMUX_PKG_REVISION="$TERMUX_PKG_REVISION".1-vheditor
+save_function termux_step_pre_configure termux_step_pre_configure_bk
+termux_step_pre_configure() {
+	CFLAGS+=" --start-no-unused-arguments -Wno-error=incompatible-function-pointer-types --end-no-unused-arguments"
+	CXXFLAGS+=" --start-no-unused-arguments -Wno-error=incompatible-function-pointer-types --end-no-unused-arguments"
+	termux_step_pre_configure_bk
+}
